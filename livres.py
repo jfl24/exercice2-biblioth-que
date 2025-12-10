4fd9b710475ba1775bab3623b451e9646a56b5b5
Samuel Lortie <samuellortie2001@gmail.com>
2025-12-10 14:42:36 -0500

    ajout de la fonction d'affichage des livres

--- livres.py
+++ livres.py
@@ -5,3 +5,8 @@ def ajouter_livre(titre, auteur):
     bibliotheque.append({"titre": titre, "auteur": auteur})
     return bibliotheque
 
+def afficher_livres():
+    for livre in bibliotheque:
+        print(f'{livre["auteur"]}:{livre["titre"]}---{livre["année"]}')
+
+afficher_livres()
