8e91b8e0690e1477c8f872ac52c8d56d033529c1
Nerkesly Duvers <nerkeslyduvers@MacBook-Air-de-Nerkesly.local>
2025-12-18 15:43:13 -0500

    dessert test

--- livres.py
+++ livres.py
@@ -19,3 +19,29 @@ def rechercher_livre(titre):
             print(f"- {livre['titre']} par {livre['auteur']} ({livre['année']})")
     else:
         print("Aucun livre trouvé avec ce titre.")
+
+import livres 
+def test_ajouter_livre():
+    livres.bibliotheque.clear()  
+    livres.ajouter_livre("1984", "George Orwell")
+    assert len(livres.bibliotheque) == 1
+    assert livres.bibliotheque[0]["titre"] == "1984"
+    assert livres.bibliotheque[0]["auteur"] == "George Orwell"
+
+def test_afficher_livres():
+    livres.bibliotheque.clear()
+    livres.ajouter_livre("Dune", "Frank Herbert")
+    print("Affichage des livres :")
+    livres.afficher_livres()  
+
+def test_rechercher_livre():
+    livres.bibliotheque.clear()
+    livres.ajouter_livre("Python", "Guido")
+    assert livres.rechercher_livre("Python") is True
+    assert livres.rechercher_livre("Java") is False
+
+if __name__ == "__main__":
+    test_ajouter_livre()
+    test_afficher_livres()
+    test_rechercher_livre()
+    print("Tous les tests ont réussi !")
