652eac4f829a951a6456dcb4c38113724fb71bad
Samuel Lortie <samuellortie2001@gmail.com>
2025-12-19 18:03:19 -0500

    finalisation de l'exercice

--- livres.py
+++ livres.py
@@ -9,7 +9,6 @@ def afficher_livres():
     for livre in bibliotheque:
         print(f'{livre["auteur"]}:{livre["titre"]}---{livre["année"]}')
 
-afficher_livres()
 
 def rechercher_livre(titre):
     resultats = [livre for livre in bibliotheque if livre['titre'].lower() == titre.lower()]
@@ -17,26 +16,29 @@ def rechercher_livre(titre):
         print("Livre(s) trouvé(s) :")
         for livre in resultats:
             print(f"- {livre['titre']} par {livre['auteur']} ({livre['année']})")
+        return True
     else:
         print("Aucun livre trouvé avec ce titre.")
+        return False
 
 import livres 
 def test_ajouter_livre():
     livres.bibliotheque.clear()  
-    livres.ajouter_livre("1984", "George Orwell")
+    livres.ajouter_livre("1984", "George Orwell", 1949)
     assert len(livres.bibliotheque) == 1
     assert livres.bibliotheque[0]["titre"] == "1984"
     assert livres.bibliotheque[0]["auteur"] == "George Orwell"
+    assert livres.bibliotheque[0]["année"] == 1949
 
 def test_afficher_livres():
     livres.bibliotheque.clear()
-    livres.ajouter_livre("Dune", "Frank Herbert")
+    livres.ajouter_livre("Dune", "Frank Herbert", 1965)
     print("Affichage des livres :")
     livres.afficher_livres()  
 
 def test_rechercher_livre():
     livres.bibliotheque.clear()
-    livres.ajouter_livre("Python", "Guido")
+    livres.ajouter_livre("Python", "Guido", 2017)
     assert livres.rechercher_livre("Python") is True
     assert livres.rechercher_livre("Java") is False
 
